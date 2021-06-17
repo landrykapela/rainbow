3b4659d3508873c4037f6a828ab7617cec951f41
Landry Kapela <landrykapela@gmail.com>
2021-06-17 16:15:08 +0300

    Add prdoduct detail and update

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 const storage = window.localStorage;
 const session = window.sessionStorage;
+
 let db = storage.getItem("db");
 if(db === undefined || db === null){
     let data = {products:[],users:[],transactions:[],inventory:[],customers:[],reps:[]};
@@ -41,9 +42,8 @@ const checkLogin = ()=>{
     }
     else{
         let user = JSON.parse(u);
-    console.log("user: ",user);
-    document.querySelector("#login").textContent = user.uname.split("@")[0];
-    return true;
+        document.querySelector("#login").textContent = user.uname.split("@")[0];
+        return true;
     }
 }
 
@@ -59,6 +59,7 @@ const showToastMessage = (message)=>{
 
 //load product list
 const listProducts = (products)=>{
+    console.log(products);
     const listContainer = document.querySelector("#list_container");
     products.forEach(p=>{
         const itemList = document.createElement("div");
@@ -100,6 +101,17 @@ const listProducts = (products)=>{
             confirmDelete("product",p.id);
         });
 
+        //edit product
+        actionEdit.addEventListener('click',(e)=>{
+            let target = window.location.protocol+"//"+window.location.host+"/edit_product.html?pid="+p.id;
+            window.location.href = target;
+        })
+
+        //item click
+        itemList.addEventListener('click',(e)=>{
+            let target = window.location.protocol+"//"+window.location.host+"/product_detail.html?pid="+p.id;
+            window.location.href = target;
+        })
     })
 }
 
@@ -141,17 +153,82 @@ const listReps = (reps)=>{
     })
 }
 
+//generate random string id;
+const randomId = (strength)=>{
+    var result           = '';
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    for ( var i = 0; i < strength; i++ ) {
+      result += characters.charAt(Math.floor(Math.random() * 
+ characters.length));
+   }
+   return result;
+}
 //confirm delete
 const confirmDelete = (itemType,itemId)=>{
     let message = "Are you sure you want to delete this item with id "+itemId+"?";
-    alert(message);
+    let return_page = "/dashboard.html";
+    if(confirm(message)){
+        switch(itemType){
+            case "product":
+                let products = db.products.filter(p=>{
+                    return p.id !== itemId;
+                });
+                db.products = products;
+                return_page = "/products.html";
+                break;
+            case "rep":
+                let reps = db.reps.filter(p=>{
+                    return p.id !== itemId;
+                });
+                db.reps = reps;
+                return_page = "/reps.html";
+                break;
+            case "customer":
+                let customers = db.customers.filter(p=>{
+                    return p.id !== itemId;
+                });
+                db.customers = customers;
+                return_page = "/customers.html";
+                break;
+            case "user":
+                let users = db.users.filter(p=>{
+                    return p.id !== itemId;
+                });
+                db.users = users;
+                return_page = "/users.html";
+                break;    
+            case "supplier":
+                let suppliers = db.suppliers.filter(p=>{
+                    return p.id !== itemId;
+                });
+                db.suppliers = suppliers;
+                return_page = "/suppliers.html";
+                break;  
+        }
+        storage.setItem("db",JSON.stringify(db));
+        window.location.pathname = return_page;
+
+    }
 }
 //save product
 const saveProduct = (product)=>{
     var products = db.products;
-    if(products.length ===0 ) product.id = 1;
-    else product.id = products[products.length - 1] + 1;
+    product.id = randomId(12);
     products.push(product);
+    db.products = products;
+    storage.setItem("db",JSON.stringify(db));
+    window.location.pathname="/products.html";
+}
+
+//update product
+const updateProduct = (product)=>{
+    var products = db.products.map(p=>{
+        if(p.id === product.id){
+            p = product;
+            
+        }
+        return p;
+    })
     db.products = products;
     storage.setItem("db",JSON.stringify(db));
     window.location.pathname="/products.html";
@@ -235,7 +312,7 @@ if(window.location.pathname == "/add_rep.html"){
             let service_area = document.getElementById("service_area").value;
             let email = document.getElementById("email").value;
             let password = document.getElementById("password").value;
-            let id = reps.length;
+            let id = randomId(12);
             let rep = {id:id,first_name:fname,last_name:lname,email:email,phone:phone,service_area:service_area,password:password};
             reps.push(rep);
             db.reps = reps;
@@ -279,4 +356,82 @@ if(window.location.pathname == "/add_product.html"){
         })
     }
 }
+}
+
+//check if current page is edit product
+if(window.location.pathname == "/edit_product.html"){
+    let login = checkLogin();
+    if(login){
+    const urlObject = new URL(window.location.href);
+    const params = urlObject.searchParams;
+    let product = db.products.filter(p=>{
+        return p.id === params.get("pid");
+    });
+    if(product.length == 0){
+        window.location.pathname = "/not_found.html";
+    }
+    else{
+        const form = document.querySelector("#edit_product_form");
+        if(form){
+            form.name.value = product[0].name;
+            form.description.value = product[0].description;
+            form.pack_size.value = product[0].pack_size;
+            document.querySelector("#preview").src = product[0].image;
+            form.addEventListener('submit',(event)=>{
+                event.preventDefault();
+                let name = document.getElementById("name").value;
+                let description = document.getElementById("description").value;
+                let pack_size = document.getElementById("pack_size").value;
+                let file = document.getElementById("image_file").files[0];
+                
+                console.log("data: ",name+"/"+description+"/"+pack_size);
+                let fileData = product[0].image;
+                let prod = product[0];
+
+                if(prod.name.toLowerCase() != name.toLowerCase()) prod.name = name;
+                if(prod.description.toLowerCase() != description.toLowerCase()) prod.description = description;
+                if(prod.pack_size != pack_size) prod.pack_size = pack_size;
+                console.log("prod: ",prod);
+                if(file){
+                    let reader = new FileReader();
+                    reader.addEventListener('load',()=>{
+                        fileData = reader.result;
+                        prod.image = fileData;
+                       updateProduct(prod);
+                    },false);
+                    reader.readAsDataURL(file);
+                }
+                else{
+                    console.log("no file");
+                    updateProduct(prod);
+                }
+               
+            })
+        }
+    }
+    
+}
+}
+
+//check if current page is prdoduct detail
+if(window.location.pathname == "/product_detail.html"){
+    let login = checkLogin();
+    if(login){
+    const urlObject = new URL(window.location.href);
+    const params = urlObject.searchParams;
+    let product = db.products.filter(p=>{
+        return p.id === params.get("pid");
+    });
+    if(product.length == 0){
+        window.location.pathname = "/not_found.html";
+    }
+    else{
+                document.getElementById("product_name").textContent = product[0].name;
+                document.getElementById("name").textContent = product[0].name;
+                document.getElementById("description").textContent = product[0].description;
+                document.getElementById("pack_size").textContent = product[0].pack_size;
+                document.getElementById("preview").src = product[0].image;
+                
+        }
+    }
 }

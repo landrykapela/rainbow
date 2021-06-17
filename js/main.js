e53f5b9bb01072bdc9108bf6038044a1e4327667
Landry Kapela <landrykapela@gmail.com>
2021-06-17 12:24:17 +0300

    Add Product feature

--- js/main.js
+++ js/main.js
@@ -8,9 +8,23 @@ if(db === undefined || db === null){
 }
 
 db = JSON.parse(db);
+
 //Functions start
 
+//encode image
+const encodeImage = (file)=>{
+    var reader = new FileReader();
+    reader.onload = function() {
 
+    var arrayBuffer = this.result,
+      array = new Uint8Array(arrayBuffer),
+      binaryString = String.fromCharCode.apply(null, array);
+      console.log(binaryString);
+    }
+    reader.readAsDataURL(file);
+    console.log("result: ",reader.result);
+    return reader.result;
+}
 //simulate page load
 const simulateLoad =(waitTime,followFunction)=>{
     setTimeout(()=>{
@@ -31,6 +45,62 @@ const checkLogin = ()=>{
     document.querySelector("#login").textContent = user.uname.split("@")[0];
     return true;
     }
+}
+
+
+//show toast
+const showToastMessage = (message)=>{
+    const msgContainer = document.createElement("div");
+    msgContainer.textContent = message;
+    msgContainer.classList.add("toast");
+    document.body.appendChild(msgContainer);
+    // msgContainer.classList.add("slideUp");
+}
+
+//load product list
+const listProducts = (products)=>{
+    const listContainer = document.querySelector("#list_container");
+    products.forEach(p=>{
+        const itemList = document.createElement("div");
+        const itemData = document.createElement("div");
+        const itemActions = document.createElement("div");
+        const title = document.createElement("span");
+        const area = document.createElement("span");
+        const actionEdit = document.createElement("span");
+        const actionDelete = document.createElement("span");
+        const image = document.createElement("img");
+
+        itemList.classList.add("item-list");
+        image.src = (p.image) ? p.image : "/img/no_data.svg";
+        image.alt = p.description;
+        image.classList.add("item-image")
+        itemList.appendChild(image);
+        area.textContent = p.pack_size;
+        title.textContent = p.name;
+        title.classList.add("item-title");
+        area.classList.add("item-focus");
+        itemData.classList.add("item-data");
+        itemData.appendChild(title);
+        itemData.appendChild(area);
+
+        itemList.appendChild(itemData);
+        itemActions.classList.add("action-buttons");
+        actionEdit.classList.add("material-icons");
+        actionEdit.textContent ="edit";
+        actionDelete.classList.add("material-icons");
+        actionDelete.textContent ="delete";
+
+        itemActions.appendChild(actionEdit);
+        itemActions.appendChild(actionDelete);
+        itemList.appendChild(itemActions);
+
+        listContainer.appendChild(itemList);
+
+        actionDelete.addEventListener('click',(e)=>{
+            confirmDelete("product",p.id);
+        });
+
+    })
 }
 
 //load reps listReps
@@ -71,6 +141,21 @@ const listReps = (reps)=>{
     })
 }
 
+//confirm delete
+const confirmDelete = (itemType,itemId)=>{
+    let message = "Are you sure you want to delete this item with id "+itemId+"?";
+    alert(message);
+}
+//save product
+const saveProduct = (product)=>{
+    var products = db.products;
+    if(products.length ===0 ) product.id = 1;
+    else product.id = products[products.length - 1] + 1;
+    products.push(product);
+    db.products = products;
+    storage.setItem("db",JSON.stringify(db));
+    window.location.pathname="/products.html";
+}
 //Functions end
 
 const loginForm = document.querySelector("#loginform");
@@ -105,15 +190,39 @@ if(window.location.pathname == "/reps.html"){
         }
         else{
            
-           simulateLoad(1000,(resps)=>{
+           simulateLoad(1000,()=>{
                listReps(reps);
            });
         }
     }
 }
 
+
+//check if current page is products.html
+if(window.location.pathname == "/products.html"){
+    let login = checkLogin();
+    if(login){
+        let products = db.products;
+        if(products.length === 0){
+            simulateLoad(1000,()=>{
+                window.location.pathname = "/add_product.html";
+            });
+            
+        }
+        else{
+           
+           simulateLoad(1000,()=>{
+               listProducts(products);
+           });
+        }
+    }
+}
+
+
 //check if current page is add reps
 if(window.location.pathname == "/add_rep.html"){
+    let login = checkLogin();
+    if(login){
     const form = document.querySelector("#new_rep_form");
     if(form){
         
@@ -134,4 +243,40 @@ if(window.location.pathname == "/add_rep.html"){
             window.location.pathname="/reps.html";
         })
     }
+}
+}
+
+//check if current page is add products
+if(window.location.pathname == "/add_product.html"){
+    let login = checkLogin();
+    if(login){
+    const form = document.querySelector("#new_product_form");
+    if(form){
+        
+        form.addEventListener('submit',(event)=>{
+            event.preventDefault();
+            let name = document.getElementById("name").value;
+            let description = document.getElementById("description").value;
+            let pack_size = document.getElementById("pack_size").value;
+            let file = document.getElementById("image_file").files[0];
+
+            let fileData = null;
+            let prod = {name:name,description:description,pack_size:pack_size,image:fileData};
+            if(file){
+                let reader = new FileReader();
+                reader.addEventListener('load',()=>{
+                    fileData = reader.result;
+                    prod.image = fileData;
+                    saveProduct(prod);
+                },false);
+                reader.readAsDataURL(file);
+            }
+            else{
+                console.log("no file");
+                saveProduct(prod);
+            }
+           
+        })
+    }
+}
 }
